9ba25edbb7c4c536878a5ef5ed01bd19b2395476
Adnan Fuentes <raydel8806@gmail.com>
2022-02-21 16:15:57 -0800

    save 01

---
+++ app/src/main/java/com/campingstudio/console/Main.kt
@@ -0,0 +1,28 @@
+val AtmMMapAccounts: MutableMap<String, Int> = mutableMapOf(
+    "0001" to 100,
+    "0002" to 100,
+    "0003" to 100)
+
+val AtmMMapAccountsReport: Map<String, Int> = AtmMMapAccounts
+
+fun extractMoney(accountId: String,cash: Int) {
+    if (AtmMMapAccounts.containsKey(accountId)) {
+        println("Extract money $accountId...")
+        AtmMMapAccounts[accountId] = AtmMMapAccounts.getValue(accountId) - cash
+    } else {
+        println("Error: not exist account (id: $accountId)")
+    }
+}
+
+fun accountsReport() {
+    println("Account balance report:")
+    AtmMMapAccountsReport.forEach { (k, v) -> println("ID $k: credit $v")}
+}
+
+fun main() {
+    accountsReport()
+    extractMoney("0001",28)
+    extractMoney("0002",50)
+    extractMoney("0005",50)
+    accountsReport()
+}
